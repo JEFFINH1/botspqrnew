1a2600c76118f3875ac95b5c98c9583724bfd622
JEFFINH1 <64107357+JEFFINH1@users.noreply.github.com>
2024-11-12 21:50:02 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -462,7 +462,7 @@ class BotController {
       // Envia os links
       await this.bot.telegram.sendMessage(
         order.chatId,
-'Grupo VIP 👇\nhttps://t.me/+e_t_9mwARJEyNTcx \n\nBrinde 1 👇\nhttps://t.me/You_Sexybeach \n\nBrinde 2 👇\nhttps://t.me/+__MUqkeNEqA1NDk0 \n\nBrinde 3 👇\nhttps://t.me/joinchat/BHQ95nfIP6YwZDk6 \n\n'
+'Grupo VIP 👇\nhttps://t.me/+2YJaTnF1kBkwN2Mx \n\nBrinde 1 👇\nhttps://t.me/You_Sexybeach \n\nBrinde 2 👇\nhttps://t.me/+__MUqkeNEqA1NDk0 \n\nBrinde 3 👇\nhttps://t.me/joinchat/BHQ95nfIP6YwZDk6 \n\n'
       )
       // Exclui a ordem do banco de dados usando o Prisma
       await this.prisma.order.delete({ where: { id: order.id } })
